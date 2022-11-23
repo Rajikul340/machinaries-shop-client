2b187b71392ee380e28638198a098e447f043253
Md Rajikul Islame <mdrajikul779@gmail.com>
2022-11-23 22:02:48 +0600

    env-fie added

--- src/Components/Firebase/Firebase.config.js
+++ src/Components/Firebase/Firebase.config.js
@@ -5,13 +5,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDxwLGJE6get0P3nQRCBzKEU9_M7_2Ky88",
-  authDomain: "machinaries-shop.firebaseapp.com",
-  projectId: "machinaries-shop",
-  storageBucket: "machinaries-shop.appspot.com",
-  messagingSenderId: "283783188536",
-  appId: "1:283783188536:web:c35f389891d8e19c96d6cd"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export default app;
